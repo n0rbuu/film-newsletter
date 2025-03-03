5d9326df560869b91c1be4c9baf52ce6924bc270
Ajayan Subramanian <ajayan.subramanian@gmail.com>
2025-03-02 19:16:05 -0500

    Added favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Between Scenes Newsletter",
   description: "A newsletter about films and cinema",
+  icons: {
+    icon: [
+      {
+        url: '/between-scenes-logo.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        url: '/between-scenes-logo.png',
+        sizes: '16x16',
+        type: 'image/png',
+      }
+    ],
+    shortcut: '/between-scenes-logo.png',
+  },
 };
 
 export default function RootLayout({
